c6fcf77ee788d318ce9787ae41d6106a8b4dca0e
Daniel Sutantyo <daniel.sutantyo@gmail.com>
2013-11-19 19:14:30 +0700

    Installed postgres (finally)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+#SampleApp::Application.config.secret_key_base = 'd9d329849709775037cdeea4be1829cf0c79e4875a4cd2a8e7e87de6cc5cf255f8493c451712cf8eaa7d5a272a638356d921314fbe3a7bc49bcf46877d682c86'
+#
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
